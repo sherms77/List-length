228250c67734db0e48a3caccdce08c7bc250509f
sherms77 <sherms77@gmail.com>
2021-02-16 10:30:36 +1100

    new file:   list_length(2).py 	modified:   notes.txt

---
+++ list_length(2).py
@@ -0,0 +1,10 @@
+t = ['spam!', ['Brie', 'Roquefort', 'Pol le Veq'], [1, 2 ,3, 4]]
+
+def list_length(l):
+    i = 0 # index
+    y = int(len(l)) # upper range limit
+    for j in range (y):
+        print(len(j))
+        i = i + 1
+
+list_length(t)
