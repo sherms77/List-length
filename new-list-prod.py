83b0722c4f731639238bee02b2913d8094e1ae2c
sherms77 <sherms77@gmail.com>
2021-02-22 11:16:37 +1100

    modified:   new-list-prod.py 	modified:   notes.txt

--- new-list-prod.py
+++ new-list-prod.py
@@ -1,33 +1,36 @@
-# loops to create new letter that can be used to a new list for each unpacked element from root list.
-
 import string
-# string.ascii_letters - upper and lower case letters
-# string.ascii_lowercase - lower case letters only
-# string.ascii_uppercase - upper case letters only
 
 l = ['spam!', 1, [2,3], 'h', 4, [5,6]]
-
+      
+# unpacks list
 def new_list_prod(r):
     z = int(len(r))
     index = 0
     v = string.ascii_letters
-        
+
+    print('\nNew list names.\nThe unpacked elements will be saved individually in these lists:')
+    for letter in range(z):
+        t = v[letter] # stores letter from v list in t variable
+        print(t) # prints letters stored in t on each iteration
+        t = [] # new lists names based on letters generated from ASCII list
+    
     print('Unpacked elements:')
     for element in range(z):
         print(r[element])
         r[element]
-    
-    # produces new list names for each unpacked element
-    print('\nElements repacked:')
-    for letter in range(z):
-        # print(v[letter]) # prints an ascii letter from list in v
 
-        t = v[letter] # stores letter from v list in t variable
-        print(t) # prints letters stored in t on each iteration
-        t = []
-        t.append(r[element]) # appends values from r list to new lists - does not work, only appends the last element?
+        # PROBLEM WITH REPACKING - REFER TO LINKS 5 and 7 in notes.txt.
+        print('\nRepacked elements:')
+        t.append(r[element]) # this seems to work but its appending each element to the next new list. It needs to save one element per list.
+        # need to use add to list not append?
         print(t) # prints values of each new list
-        
+    
 new_list_prod(l)
-            
-            
+
+'''
+# saves individual lists as nested elements in aggregate list
+def aggregate_list():
+    pass 
+'''
+
+      
